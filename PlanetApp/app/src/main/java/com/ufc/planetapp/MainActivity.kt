7805781c1cb88c9eb708c19b83d7319c31d6a950
CristianoMends <mendescristiano012@gmail.com>
2025-06-26 20:07:08 -0300

    add PlanetApp

---
+++ PlanetApp/app/src/main/java/com/ufc/planetapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ufc.planetapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ufc.planetapp.navigation.NavGraph
+import com.ufc.planetapp.ui.theme.PlanetAppTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavGraph(
+                onSettingsClick = {},
+                onHelpClick = { },
+            )
+        }
+    }
+}
