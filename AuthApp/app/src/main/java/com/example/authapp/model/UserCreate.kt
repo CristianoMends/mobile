6b1be7945785c6b11f9b7ef91d67e6ee3df3de70
CristianoMends <mendescristiano012@gmail.com>
2025-06-27 18:42:04 -0300

    add AuthApp

---
+++ AuthApp/app/src/main/java/com/example/authapp/model/UserCreate.kt
@@ -0,0 +1,3 @@
+package com.example.authapp.model
+
+data class UserCreate(val email: String, val password: String)
---
+++ AuthApp/app/src/main/java/com/example/authapp/model/UserOut.kt
@@ -0,0 +1,3 @@
+package com.example.authapp.model
+
+data class UserOut(val id: Int, val email: String)
